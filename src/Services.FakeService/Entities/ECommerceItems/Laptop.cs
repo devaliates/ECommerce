189d5ee19ab56cf67dc8cb6927c47a15ecf8b051
Ali Ateş <devaliates@gmail.com>
2021-07-04 22:19:54 +0300

    Services.FakeService projesi eklendi.

---
+++ src/Services.FakeService/Entities/ECommerceItems/Laptop.cs
@@ -0,0 +1,15 @@
+﻿using Core.Interfaces.ECommerceItems;
+
+using System;
+
+namespace Services.FakeService.Entities.ECommerceItems
+{
+    public class Laptop : ILaptop
+    {
+        public string Title { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public Guid Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool IsSellable { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public decimal Price { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public short Count { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
---
+++ src/Services.FakeService/LaptopService.cs
@@ -0,0 +1,15 @@
+﻿using Core.Interfaces.DataLayer;
+
+using DataLayer.FakeDatabase;
+
+using Services.FakeService.Entities.ECommerceItems;
+
+using System;
+
+namespace Services.FakeService
+{
+    public class LaptopService
+    {
+        private ILaptopDataLayer<Laptop, Guid> service = new LaptopDataLayer<Laptop, Guid>();
+    }
+}
