1c1137478a269d4162007258fadf8d1abf3a1dff
Ali Ateş <devaliates@gmail.com>
2021-07-04 22:14:43 +0300

    DataLayer.FakeDatabase projesi eklendi.

---
+++ src/DataLayer.FakeDatabase/LaptopDataLayer.cs
@@ -0,0 +1,41 @@
+﻿using Core.Interfaces.DataLayer;
+using Core.Interfaces.ECommerceItems;
+
+using System;
+using System.Threading.Tasks;
+
+namespace DataLayer.FakeDatabase
+{
+    public class LaptopDataLayer<T, TKey> : ILaptopDataLayer<T, TKey> where T : ILaptop
+    {
+        public T Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> AddAsync(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(T key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetAsync(TKey key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Remove(TKey key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> RemoveAsync(TKey key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
